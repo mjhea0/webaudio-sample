e8dbb6c12739a45d9649b57e4708539fff575812
Michael Herman <hermanmu@gmail.com>
2013-12-05 00:21:05 -0700

    initial

---
+++ main.js
@@ -0,0 +1,44 @@
+var url = 'sample.mp3';
+var source;
+var context = new webkitAudioContext();
+var analyser = context.createAnalyser();
+
+function playSound(buffer) {
+  source = context.createBufferSource();
+  source.buffer = buffer;
+  source.connect(analyser);
+  analyser.connect(context.destination);
+  source.noteOn(0);
+};
+
+function request() {
+  var request = new XMLHttpRequest();
+  request.open('GET', url, true);
+  request.responseType = 'arraybuffer';
+  request.onload = function() {
+    context.decodeAudioData(request.response, function(buffer) {
+      // play!
+      console.log("playing ...")
+      playSound(buffer);
+    });
+  };
+  request.send();
+};
+
+// go!
+request();
+
+// countdown
+$(function(){
+  var timer = setInterval(function(){
+    $(".count").html(function(i,html){
+      if(parseInt(html)>1) {
+        return parseInt(html)-1;
+      } else {
+        clearTimeout(timer);
+        return "play!!";
+      }
+    });
+  },1500);
+});
+
---
+++ readme.md
@@ -0,0 +1,9 @@
+## setup
+
+1. clone the repo:
+2. navigate into directory: `$ cd webaudio-sample`
+3. launch a python server: `$ python -m SimpleHTTPServer`
+4. navigate to [http://localhost:8000/](http://localhost:8000/)
+5. enjoy
+
+.. or just listen [here](link)
